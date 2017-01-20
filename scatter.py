d2c675265e45d9042c7abad9a515e3ce12142df0
Kayleen Panlilio <kpanlilio@ucvts.org>
2017-01-20 01:08:26 +0000

    created scatter.py and scatter plot

---
+++ scatter.py
@@ -0,0 +1,36 @@
+# module
+import csv
+import pandas as pd
+import numpy as np
+import bokeh
+
+# read and get data
+df = pd.read_csv('Pokemon.csv')
+
+# import necessary common inputs for visual
+from bokeh.charts import Scatter, output_file, save
+
+# create the scatter chart
+p = Scatter(df, x='Attack', y='Defense', title="Attack vs. Defense Stats", xlabel="Attack", ylabel="Defense")
+
+# create output file
+output_file('scatter.html')
+# save graph to output file
+save(p)
+
+
+# #import module
+# import csv
+# import pandas as pd
+# import numpy as np
+# import bokeh
+
+# # read data
+# df = pd.read_csv('Pokemon.csv')
+
+# from bokeh.charts import Bar, output_file, save
+
+# graph = Bar(df, 'Type 1', values='#', title='Frequency of Pokemon Types', color='pink')
+
+# output_file('scatterplot.html')
+# save(graph)

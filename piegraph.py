ce9ef801c87f0ae78e7ba3d4394ca286dedd89fe
Kayleen Panlilio <kpanlilio@ucvts.org>
2017-01-20 00:11:37 +0000

    added comments to piegraph.py

--- piegraph.py
+++ piegraph.py
@@ -1,31 +1,21 @@
-#import module
+# import modules
 import csv
 import pandas as pd
 import bokeh
 
-# read data
+# read csv dataframe
 df = pd.read_csv('Pokemon.csv')
 
+# import necessary plotting imports
 from bokeh.charts import Donut, output_file, save
-
+# import color palette 
 from bokeh.palettes import plasma
 
+# set up the bar graph components
 sab = Donut(df, label='Type 1', values='#', title='Frequency of Pokemon Types', text_font_size='7pt', hover_text='#', palette=plasma(18))
 
+# create output file
 output_file('piegraph.html')
+# save the pie graph to file
 save(sab)
 
-# #import module
-# import csv
-# import pandas as pd
-# import numpy as np
-# import bokeh
-
-# # read data
-# df = pd.read_csv('Pokemon.csv')
-
-# from bokeh.charts import Donut, output_file, save
-# sab = Bar(df, 'Type 1', values='#', title='Frequency of Pokemon Types', color='pink')
-
-# output_file('sab.html')
-# save(sab)

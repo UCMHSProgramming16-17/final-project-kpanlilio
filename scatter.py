eec65036c13a20e426acb409d9d4a4f79afe632e
Kayleen Panlilio <kpanlilio@ucvts.org>
2017-01-21 19:48:00 +0000

    finalized scatter.html and scatter.py

--- scatter.py
+++ scatter.py
@@ -10,28 +10,18 @@ df = pd.read_csv('Pokemon.csv')
 # import necessary common inputs for visual
 from bokeh.charts import Scatter, output_file, save
 
+# add information to the hover tool
+tooltips=[
+    ('Name', '@Name'),
+    ('Attack Stat', '@Attack'),
+    ('Defense Stat', '@Defense')
+]
+
 # create the scatter chart
 p = Scatter(df, x='Attack', y='Defense', title="Attack vs. Defense Stats", 
-            xlabel="Attack", ylabel="Defense", color='Type 1', marker='Type 1')
+            xlabel="Attack", ylabel="Defense", color='Type 1', tooltips=tooltips)
 
 # create output file
 output_file('scatter.html')
 # save graph to output file
 save(p)
-
-
-# #import module
-# import csv
-# import pandas as pd
-# import numpy as np
-# import bokeh
-
-# # read data
-# df = pd.read_csv('Pokemon.csv')
-
-# from bokeh.charts import Bar, output_file, save
-
-# graph = Bar(df, 'Type 1', values='#', title='Frequency of Pokemon Types', color='pink')
-
-# output_file('scatterplot.html')
-# save(graph)

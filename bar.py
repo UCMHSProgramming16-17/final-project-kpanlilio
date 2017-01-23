b30a0e4b6da45cbda32907ec12b0edc646bfcfa3
Kayleen Panlilio <kpanlilio@ucvts.org>
2017-01-23 04:44:09 +0000

    created bar graph and edited bar.py

--- bar.py
+++ bar.py
@@ -1,7 +1,20 @@
+# import necessary modules
 import csv
 import pandas as pd
 import bokeh
 
+# import/ read the csv file
 df = pd.read_csv('Pokemon.csv')
 
-from bokeh.charts import Histogram, output_file, save
+# import the necessary visual imports
+from bokeh.charts import Bar, output_file, save
+
+# set up the bar graph visuals
+p = Bar(df, 'Generation', color='Generation', legend='top_right', title="Number of Pokemon per Generation")
+
+# create the output file
+output_file("bar.html")
+
+# save bar graph to output file
+save(p)
+
